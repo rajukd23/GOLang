2e9668f7b33c01d609fbd634e183445444161a25
rajukd23 <59725971+rajukd23@users.noreply.github.com>
2020-04-12 19:24:18 -0700

    Add files via upload
    client server text exchange.. Chatbox.

---
+++ TicTacToe/client.cpp
@@ -0,0 +1,60 @@
+
+// Client side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT     8080
+#define MAXLINE 1024
+
+// Driver code
+int main() {
+    int sockfd;
+    char buffer[MAXLINE];
+    //char *hello = "Hello from client";
+    //char hello[MAXLINE];
+    char *hello = NULL;
+    struct sockaddr_in servaddr;
+
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    int n;
+    socklen_t len;
+    size_t inputLen =0;
+   while (strlen(buffer)!=1) {
+    printf("HEY :Enter next string -- ");
+    //scanf("%s", hello);
+    getline(&hello, &inputLen, stdin);
+    printf("length of scanned word %u\n", strlen(hello));
+    sendto(sockfd, (const char *)hello, strlen(hello),
+        MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+            sizeof(servaddr));
+    printf("Hello message sent.\n");
+
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+                MSG_WAITALL, (struct sockaddr *) &servaddr,
+                &len);
+    buffer[n] = '\0';
+    printf("Server : %s\n", buffer);
+
+   } // end while..
+
+    close(sockfd);
+    return 0;
+}
