47982fef1d0c4e0228c5a98fee995d32c0a6b351
rajukd23 <59725971+rajukd23@users.noreply.github.com>
2020-04-12 22:31:16 -0700

    Add files via upload
    simple TTT Client server

--- TicTacToe/client.cpp
+++ TicTacToe/client.cpp
@@ -12,6 +12,51 @@
 #define PORT     8080
 #define MAXLINE 1024
 
+
+// structure for TICTACTOE
+char array[9];
+
+
+char* validateInput(char *hello, size_t inputLen)
+{
+	while(1){
+		printf("HEY :Enter a number -- ");
+		getline(&hello, &inputLen, stdin);
+//		printf("strelen %u\n", strlen(hello));
+		printf("hello[0] %c\n", hello[0]);
+
+		if(strlen(hello)==2 && hello[0]>=49 && hello[0]<=57)
+			return hello;
+	}
+
+}
+
+void initArray() {
+	for (int i=0;i< 9;i++)
+		array[i] = '-';
+}
+
+void displayBoard()
+{
+	printf("Client (X) and Server (O)\n");
+	for (int i =0; i<9;i++){
+		if((i)%3 ==0) printf("\n");
+		printf("\t %c", array[i]);
+	}
+	printf("\n");
+
+}
+
+// update Array with the provided numbers
+void manipulateBoard(char *c, char source)
+{
+	int i;
+	sscanf(c, "%d", &i); // Using sscanf
+	array[i-1] = source;
+
+	displayBoard();
+}
+
 // Driver code
 int main() {
     int sockfd;
@@ -26,6 +71,11 @@ int main() {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
+    printf("CLIENT : CONNECTION ESTABISHED\n" );
+
+    printf("Intializing the board\n");
+    initArray();
+    displayBoard();
 
     memset(&servaddr, 0, sizeof(servaddr));
 
@@ -37,22 +87,25 @@ int main() {
     int n;
     socklen_t len;
     size_t inputLen =0;
-   while (strlen(buffer)!=1) {
-    printf("HEY :Enter next string -- ");
-    //scanf("%s", hello);
-    getline(&hello, &inputLen, stdin);
-    printf("length of scanned word %u\n", strlen(hello));
+
+   while (strlen(buffer)!=1 && buffer[0] !=0) {
+
+    hello = validateInput(hello, inputLen);
+
+    printf("Entered number %s\n", hello);
     sendto(sockfd, (const char *)hello, strlen(hello),
         MSG_CONFIRM, (const struct sockaddr *) &servaddr,
             sizeof(servaddr));
-    printf("Hello message sent.\n");
+    manipulateBoard(hello, 'X');
+
+    printf("message sent.\n");
 
     n = recvfrom(sockfd, (char *)buffer, MAXLINE,
                 MSG_WAITALL, (struct sockaddr *) &servaddr,
                 &len);
     buffer[n] = '\0';
     printf("Server : %s\n", buffer);
-
+    manipulateBoard(buffer, 'O');
    } // end while..
 
     close(sockfd);
